8cacd5fbbd13f3003ffdb7680814a967088c6bc0
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-11-22 18:07:55 -0500

    Style

--- src/App.js
+++ src/App.js
@@ -40,20 +40,20 @@ function App () {
           <h1>QR GENERATOR</h1>
       </div>
       <div className='wrapper'>
-        <input className='input' style={{padding:"20px 140px 20px 30px" , width:"25rem", borderRadius:"30px", border:"none", background:"#ffffff", marginTop:"150px", fontFamily:"sans-serif", fontSize:"16px"}} placeholder='Enter a valid URL' onChange={e => handleChange(e)} />
+        <input className='input' style={{padding:"20px 140px 20px 30px" , width:"25rem", borderRadius:"30px", border:"none", background:"#ffffff", marginTop:"150px", fontFamily:"sans-serif", fontSize:"16px", fontWeight:"600"}} placeholder='Enter a valid URL' onChange={e => handleChange(e)} />
         <button className='btn' onClick={() => handleSubmit()} style={{margin:"-50px 0 0 17.2rem", borderRadius:"30px", padding:"15px 20px", border:"none", fontFamily:"sans-serif", fontSize:"12px", fontWeight:"600" , background:"#ff7b73", color:"#fff"}} >Generate QR</button>
         <p style={{height:"50px", marginTop:"30px", color:"#fff"}}>{ show && errorMessage}</p>
       </div>
       <div style={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", width:"100%"}}>
         {qrArray.length > 0 && (
-          <p style={{fontFamily:"sans-serif", fontSize:"12px", fontFamily:"sans-serif", color:"#fff"}}>
-              Left click & "save image as" to download your QR.
-          </p>
+            <p style={{fontFamily:"sans-serif", fontSize:"12px", fontFamily:"sans-serif", color:"#fff"}}>
+                Left click & "save image as" to download your QR.
+            </p>
         )}
         {qrArray.map((code, index) => (
           <div key={`${code.Url}${index + 1}`} style={{display:"flex", flexDirection:"column", justifyContent:"space-between", alignItems:"center", width:"100%", margin:"50px 0"}}>
             <img style={{width:"200px"}} src={code.dataUrl} alt="qr code"/>
-            <p style={{fontFamily:"sans-serif", fontSize:"16px", fontWeight:"600", marginTop:"20px"}}>{code.Url}</p>
+            <p style={{fontFamily:"sans-serif", fontSize:"16px", fontWeight:"600", marginTop:"20px", color:"#ffcccc"}}>{code.Url}</p>
           </div>
         ))}
       </div>
